f98363ec3161d111275291de76047073516327c4
Prashant <hadaprashant2002@gmail.com>
2024-09-16 13:58:29 +0530

    added support for ls-tree command

--- app/git/commands/index.js
+++ app/git/commands/index.js
@@ -1,7 +1,9 @@
 const CatFile = require("./cat-file.js");
 const HashObject = require("./hash-object.js");
+const LsTree = require("./ls-tree.js")
 
 module.exports = {
     CatFile,
-    HashObject
+    HashObject,
+    LsTree
 }
