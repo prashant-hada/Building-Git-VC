178cdb098401de2dc68af429f69233a725f9b049
Prashant <hadaprashant2002@gmail.com>
2024-09-16 17:41:52 +0530

    write-tree command added

--- app/git/commands/index.js
+++ app/git/commands/index.js
@@ -1,9 +1,11 @@
 const CatFile = require("./cat-file.js");
 const HashObject = require("./hash-object.js");
-const LsTree = require("./ls-tree.js")
+const LsTree = require("./ls-tree.js");
+const WriteTree = require("./write-tree.js");
 
 module.exports = {
     CatFile,
     HashObject,
-    LsTree
+    LsTree,
+    WriteTree
 }
